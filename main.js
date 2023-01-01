5692bbc5ec469c9894992d12ba37ba5031823cdd
ElyorxonB <117801522+ElyorxonB@users.noreply.github.com>
2023-01-01 08:55:19 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,307 @@
+var age = +prompt('Введите ваш возраст');
+
+if(age === 1) {
+    console.log((+age) + ' вам нужно учиться');
+    alert('Вам нужно учиться')
+} else if(age === 2) {
+    console.log((+age) + ' вам нужно учиться');
+    alert('Вам нужно учиться')
+}else if(age === 3) {
+    console.log((+age) + ' вам нужно учиться');
+    alert('Вам нужно учиться')
+}else if(age === 4) {
+    console.log((+age) + ' вам нужно учиться');
+    alert('Вам нужно учиться')
+}else if(age === 5) {
+    console.log((+age) + ' вам нужно учиться');
+    alert('Вам нужно учиться')
+}else if(age === 6) {
+    console.log((+age) + ' вам нужно учиться');
+    alert('Вам нужно учиться')
+}else if(age === 7) {
+    console.log((+age) + ' вам нужно учиться');
+    alert('Вам нужно учиться')
+}else if(age === 8) {
+    console.log((+age) + ' вам нужно учиться');
+    alert('Вам нужно учиться')
+}else if(age === 9) {
+    console.log((+age) + ' вам нужно учиться');
+    alert('Вам нужно учиться')
+}else if(age === 10) {
+    console.log((+age) + ' вам нужно учиться');
+    alert('Вам нужно учиться')
+}else if(age === 11) {
+    console.log((+age) + ' вам нужно учиться');
+    alert('Вам нужно учиться')
+}else if(age === 12) {
+    console.log((+age) + ' вам нужно учиться');
+    alert('Вам нужно учиться')
+}else if(age === 13) {
+    console.log((+age) + ' вам нужно учиться');
+    alert('Вам нужно учиться')
+}else if(age === 14) {
+    console.log((+age) + ' вам нужно учиться');
+    alert('Вам нужно учиться')
+}else if(age === 15) {
+    console.log((+age) + ' вам нужно учиться');
+    alert('Вам нужно учиться')
+}else if(age === 16) {
+    console.log((+age) + ' вам нужно учиться');
+    alert('Вам нужно учиться')
+}else if(age === 17) {
+    console.log((+age) + ' вам нужно учиться');
+    alert('Вам нужно учиться')
+}else if(age === 18) {
+    console.log((+age) + ' вам нужно учиться');
+    alert('Вам нужно учиться')
+}else if(age === 19) {
+    console.log((+age) + ' вам нужно Работать');
+    alert('Вам нужно Работать')
+}else if(age === 20) {
+    console.log((+age) + ' вам нужно Работать');
+    alert('Вам нужно Работать')
+}else if(age === 21) {
+    console.log((+age) + ' вам нужно Работать');
+    alert('Вам нужно Работать')
+}else if(age === 22) {
+    console.log((+age) + ' вам нужно Работать');
+    alert('Вам нужно Работать')
+}else if(age === 23) {
+    console.log((+age) + ' вам нужно Работать');
+    alert('Вам нужно Работать')
+}else if(age === 24) {
+    console.log((+age) + ' вам нужно Работать');
+    alert('Вам нужно Работать')
+}else if(age === 25) {
+    console.log((+age) + ' вам нужно Работать');
+    alert('Вам нужно Работать')
+}else if(age === 26) {
+    console.log((+age) + ' вам нужно Работать');
+    alert('Вам нужно Работать')
+}else if(age === 27) {
+    console.log((+age) + ' вам нужно Работать');
+    alert('Вам нужно Работать')
+}else if(age === 28) {
+    console.log((+age) + ' вам нужно Работать');
+    alert('Вам нужно Работать')
+}else if(age === 29) {
+    console.log((+age) + ' вам нужно Работать');
+    alert('Вам нужно Работать')
+}else if(age === 30) {
+    console.log((+age) + ' вам нужно Работать');
+    alert('Вам нужно Работать')
+}else if(age === 31) {
+    console.log((+age) + ' вам нужно Работать');
+    alert('Вам нужно Работать')
+}else if(age === 32) {
+    console.log((+age) + ' вам нужно Работать');
+    alert('Вам нужно Работать')
+}else if(age === 33) {
+    console.log((+age) + ' вам нужно Работать');
+    alert('Вам нужно Работать')
+}else if(age === 34) {
+    console.log((+age) + ' вам нужно Работать');
+    alert('Вам нужно Работать')
+}else if(age === 35) {
+    console.log((+age) + ' вам нужно Работать');
+    alert('Вам нужно Работать')
+}else if(age === 36) {
+    console.log((+age) + ' вам нужно Работать');
+    alert('Вам нужно Работать')
+}else if(age === 37) {
+    console.log((+age) + ' вам нужно Работать');
+    alert('Вам нужно Работать')
+}else if(age === 38) {
+    console.log((+age) + ' вам нужно Работать');
+    alert('Вам нужно Работать')
+}else if(age === 39) {
+    console.log((+age) + ' вам нужно Работать');
+    alert('Вам нужно Работать')
+}else if(age === 40) {
+    console.log((+age) + ' вам нужно Работать');
+    alert('Вам нужно Работать')
+}else if(age === 41) {
+    console.log((+age) + ' вам нужно Работать');
+    alert('Вам нужно Работать')
+}else if(age === 42) {
+    console.log((+age) + ' вам нужно Работать');
+    alert('Вам нужно Работать')
+}else if(age === 43) {
+    console.log((+age) + ' вам нужно Работать');
+    alert('Вам нужно Работать')
+}else if(age === 44) {
+    console.log((+age) + ' вам нужно Работать');
+    alert('Вам нужно Работать')
+}else if(age === 45) {
+    console.log((+age) + ' вам нужно Работать');
+    alert('Вам нужно Работать')
+}else if(age === 46) {
+    console.log((+age) + ' вам нужно Работать');
+    alert('Вам нужно Работать')
+}else if(age === 47) {
+    console.log((+age) + ' вам нужно Работать');
+    alert('Вам нужно Работать')
+}else if(age === 48) {
+    console.log((+age) + ' вам нужно Работать');
+    alert('Вам нужно Работать')
+}else if(age === 49) {
+    console.log((+age) + ' вам нужно Работать');
+    alert('Вам нужно Работать')
+}else if(age === 50) {
+    console.log((+age) + ' вам нужно Работать');
+    alert('Вам нужно Работать')
+}else if(age === 51) {
+    console.log((+age) + ' Вам скоро на пенсию');
+    alert('Вам скоро на пенсию')
+}else if(age === 52) {
+    console.log((+age) + ' Вам скоро на пенсию');
+    alert('Вам скоро на пенсию')
+}else if(age === 53) {
+    console.log((+age) + ' Вам скоро на пенсию');
+    alert('Вам скоро на пенсию')
+}else if(age === 54) {
+    console.log((+age) + ' Вам скоро на пенсию');
+    alert('Вам скоро на пенсию')
+}else if(age === 55) {
+    console.log((+age) + ' Вам скоро на пенсию');
+    alert('Вам скоро на пенсию')
+}else if(age === 56) {
+    console.log((+age) + ' Вам скоро на пенсию');
+    alert('Вам скоро на пенсию')
+}else if(age === 57) {
+    console.log((+age) + ' Вам скоро на пенсию');
+    alert('Вам скоро на пенсию')
+}else if(age === 58) {
+    console.log((+age) + ' Вам скоро на пенсию');
+    alert('Вам скоро на пенсию')
+}else if(age === 59) {
+    console.log((+age) + ' Вам скоро на пенсию');
+    alert('Вам скоро на пенсию')
+}else if(age === 60) {
+    console.log((+age) + ' Вы пенсионер');
+    alert('Вы пенсионер')
+} else if(age === 61) {
+    console.log((+age) + ' Вы пенсионер');
+    alert('Вы пенсионер')
+}else if(age === 62) {
+    console.log((+age) + ' Вы пенсионер');
+    alert('Вы пенсионер')
+}else if(age === 63) {
+    console.log((+age) + ' Вы пенсионер');
+    alert('Вы пенсионер')
+}else if(age === 64) {
+    console.log((+age) + ' Вы пенсионер');
+    alert('Вы пенсионер')
+}else if(age === 65) {
+    console.log((+age) + ' Вы пенсионер');
+    alert('Вы пенсионер')
+}else if(age === 66) {
+    console.log((+age) + ' Вы пенсионер');
+    alert('Вы пенсионер')
+}else if(age === 67) {
+    console.log((+age) + ' Вы пенсионер');
+    alert('Вы пенсионер')
+}else if(age === 68) {
+    console.log((+age) + ' Вы пенсионер');
+    alert('Вы пенсионер')
+}else if(age === 69) {
+    console.log((+age) + ' Вы пенсионер');
+    alert('Вы пенсионер')
+}else if(age === 70) {
+    console.log((+age) + ' Вы пенсионер');
+    alert('Вы пенсионер')
+}else if(age === 71) {
+    console.log((+age) + ' Вы пенсионер');
+    alert('Вы пенсионер')
+}else if(age === 72) {
+    console.log((+age) + ' Вы пенсионер');
+    alert('Вы пенсионер')
+}else if(age === 73) {
+    console.log((+age) + ' Вы пенсионер');
+    alert('Вы пенсионер')
+}else if(age === 74) {
+    console.log((+age) + ' Вы пенсионер');
+    alert('Вы пенсионер')
+}else if(age === 75) {
+    console.log((+age) + ' Вы пенсионер');
+    alert('Вы пенсионер')
+}else if(age === 76) {
+    console.log((+age) + ' Вы пенсионер');
+    alert('Вы пенсионер')
+}else if(age === 77) {
+    console.log((+age) + ' Вы пенсионер');
+    alert('Вы пенсионер')
+}else if(age === 78) {
+    console.log((+age) + ' Вы пенсионер');
+    alert('Вы пенсионер')
+}else if(age === 79) {
+    console.log((+age) + ' Вы пенсионер');
+    alert('Вы пенсионер')
+}else if(age === 80) {
+    console.log((+age) + ' Вы пенсионер');
+    alert('Вы пенсионер')
+}else if(age === 81) {
+    console.log((+age) + ' Вы пенсионер');
+    alert('Вы пенсионер')
+}else if(age === 82) {
+    console.log((+age) + ' Вы пенсионер');
+    alert('Вы пенсионер')
+}else if(age === 83) {
+    console.log((+age) + ' Вы пенсионер');
+    alert('Вы пенсионер')
+}else if(age === 84) {
+    console.log((+age) + ' Вы пенсионер');
+    alert('Вы пенсионер')
+}else if(age === 85) {
+    console.log((+age) + ' Вы пенсионер');
+    alert('Вы пенсионер')
+}else if(age === 86) {
+    console.log((+age) + ' Вы пенсионер');
+    alert('Вы пенсионер')
+}else if(age === 87) {
+    console.log((+age) + ' Вы пенсионер');
+    alert('Вы пенсионер')
+}else if(age === 88) {
+    console.log((+age) + ' Вы пенсионер');
+    alert('Вы пенсионер')
+}else if(age === 89) {
+    console.log((+age) + ' Вы пенсионер');
+    alert('Вы пенсионер')
+}else if(age === 90) {
+    console.log((+age) + ' Вы пенсионер');
+    alert('Вы пенсионер')
+}else if(age === 91) {
+    console.log((+age) + ' Вы пенсионер');
+    alert('Вы пенсионер')
+}else if(age === 92) {
+    console.log((+age) + ' Вы пенсионер');
+    alert('Вы пенсионер')
+}else if(age === 93) {
+    console.log((+age) + ' Вы пенсионер');
+    alert('Вы пенсионер')
+}else if(age === 94) {
+    console.log((+age) + ' Вы пенсионер');
+    alert('Вы пенсионер')
+}else if(age === 95) {
+    console.log((+age) + ' Вы пенсионер');
+    alert('Вы пенсионер')
+}else if(age === 96) {
+    console.log((+age) + ' Вы пенсионер');
+    alert('Вы пенсионер')
+}else if(age === 97) {
+    console.log((+age) + ' Вы пенсионер');
+    alert('Вы пенсионер')
+}else if(age === 98) {
+    console.log((+age) + ' Вы пенсионер');
+    alert('Вы пенсионер')
+}else if(age === 99) {
+    console.log((+age) + ' Вы пенсионер');
+    alert('Вы пенсионер')
+}else if(age === 100) {
+    console.log((+age) + ' Вы пенсионер');
+    alert('Вы пенсионер')
+}
+else {
+    console.log('R.I.P')
+    alert('R.I.P')
+}
